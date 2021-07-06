f9fed2fa503135743a098323ebe1196540ded5e2
jairosr <jairosaucedo.ruiz@gmail.com>
2021-07-06 01:52:36 -0500

    agregamos html,css y js

---
+++ script.js
@@ -0,0 +1,23 @@
+function selectColor(){
+    const change = document.getElementById("color").value;
+}
+
+addEventListener('click', evento => {
+    const colores = document.getElementById("color").value;
+
+    if(colores === 'yellow'){
+        evento.target.style.backgroundColor = 'yellow';
+    }
+    if(colores === 'blue'){
+        evento.target.style.backgroundColor = 'blue';
+    }
+    if(colores === 'green'){
+        evento.target.style.backgroundColor = 'green';
+    }
+    if(colores === 'pink'){
+        evento.target.style.backgroundColor = 'pink';
+    }  
+    if(colores === 'orange'){
+        evento.target.style.backgroundColor = 'orange';
+    }   
+})
